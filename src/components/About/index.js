f4219c5376a7f7f2f84a4bb500321c28dc5934ee
thisisjona <thisisjona@gmail.com>
2022-05-15 12:53:23 -0500

    updated about me/footer

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,10 @@ import profilePic from '../../assets/images/react-prof-headshot.png';
 
 function About() {
     return(
-      <div> 
+      <div className="my-5"> 
+        <h1>About Me:</h1>
          <img src={profilePic} alt='profile pic'/> 
-         <p>lorem ipsum que hubole con todos los perrinkis trinkis</p>
+         <p>My name is Jonathan Rivera. I arrived in Austin, Texas in 2011 to pursue a musical education and carreer. Throughout my education, I facilitated my studies by working in the service industry, which has given me a deep skillset for interpersonal relationships. As the Capital of Texas has evolved, so has my interest in Web Development. I am looking to grown in the tech field by expanding my skillset in the workforce by working on projects and joining a company with vision and integrity parallel to mine</p>
       </div>
     )
 }
